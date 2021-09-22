dff6f0284e67f5090798dd0912111490c0c8ed98
Armando <armandoramossanchez@gmail.com>
2021-09-22 12:31:55 +0200

    subo reto 3

---
+++ Reto 3/js/script.js
@@ -0,0 +1,33 @@
+//Reto 3
+
+/*
+Crea una aplicación que nos pida un número por prompt y con un método se lo
+pasamos por parámetro para que nos indique si es o no un número primo, debe
+devolver true si es primo sino false. Un número primo es aquel solo puede dividirse
+entre 1 y sí mismo. Por ejemplo: 25 no es primo, ya que 25 es divisible entre 5, sin
+embargo, 17 si es primo.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var numero = prompt("Introduce un número para saber si es primo");
+numero = parseInt(numero);
+var primo = true;
+
+//Proceso
+
+for (let i = 2; i < numero; i++) {
+    if (numero%i == 0) {
+        primo = false;
+    }
+}
+
+//Output
+
+console.log(primo);
+
