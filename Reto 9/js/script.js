25f001074de69f743f26165e98f3fb9f159c1f87
Armando <armandoramossanchez@gmail.com>
2021-09-22 13:56:23 +0200

    subo reto 9

---
+++ Reto 9/js/script.js
@@ -0,0 +1,36 @@
+//Reto 9
+
+/*
+Crea un array de números donde le indicamos por prompt el tamaño del array,
+rellenaremos el array con números aleatorios entre 0 y 9. Al final muestra por consola
+el valor de cada posición y la suma de todos los valores. Tareas a realizar: Haz un
+método para rellenar el array(que tenga como parámetros los números entre los que
+tenga que generar) y otro para mostrar el contenido y la suma del array.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var array = [];
+var tamaño = prompt("Introduce el tamaño del array");
+tamaño = parseInt(tamaño);
+var suma = 0;
+
+//Proceso
+
+for (let i = 1; i <= tamaño; i++) {
+    array.push(Math.random() * (10 - 0));
+}
+
+array.forEach(numero => {
+    suma = suma + numero;
+});
+
+//Output
+
+console.log(array);
+console.log(suma);
